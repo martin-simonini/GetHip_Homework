6b1648891410e7b69e8c526b2e26553a0d53dcba
martin-simonini <soy_martin98@yahoo.com>
2015-09-26 14:49:56 -0500

    Finished it

--- Bank.java
+++ Bank.java
@@ -2,19 +2,70 @@ import java.util.Scanner;
 
 public class Bank
 {
- int pin = 1998;
- boolean correctPin = false;
- 
- Scanner sc = new Scanner(System.in);
- 
- while(correctPin != true)
- {
-  System.out.print("Please enter your four digit pin: ");
-  int input = sc.nextInt();
-  if(input == 1998)
-  {
-   correctPin = true;
-  }
+
+   public static void main(String[] args)
+   {
+
+   //---------Variable Declaration------------
+  int pin = 1998;
+  boolean correctPin = false;
+ double balance = 0.0; //the balance of the user
+   boolean exit = false;
+
+   Scanner sc = new Scanner(System.in);
+  
+   while(correctPin == false)
+   {
+
+      System.out.print("Please enter your four digit pin: ");
+            int PinInput = sc.nextInt();
+      if(PinInput == 1998)
+      {
+       correctPin = true;
+      }
+      else
+     {
+      System.out.println("Incorrect Pin");
+     }
  }
+
+ while(exit != true)
+ {
+
+ System.out.println("What would you like to do today?");
+ System.out.println("1. View balance");
+ System.out.println("2. Deposit money");
+ System.out.println("3. Withdrawal money");
+ System.out.println("4. Exit");
+ 
+ int  choice = sc.nextInt();
+
+ if(choice ==1)
+ {
+   System.out.println("$"+balance);
+ }
+ else if(choice == 2) //deposit
+ {
+    System.out.print("How much would you like to deposit?");
+    int depo = sc.nextInt();
+    balance += depo;
+ }
+ else if(choice == 3) //withdrawal
+ {
+   System.out.print("How much would you like to withdraw?");
+   int with = sc.nextInt();
+   if(balance - with < 0)
+   {
+    System.out.println("You do not have the sufficent funds.");
+    continue;
+   }
+ 
+ }
+ else //exit
+    exit = true;
+   }
+
+
+  }
 
 }
