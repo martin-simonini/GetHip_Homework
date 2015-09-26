a17c25b0a6039334a1a565cfa09ac85cb017ee14
martin-simonini <soy_martin98@yahoo.com>
2015-09-26 11:50:35 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Bank
 {
+ int pin = 1998;
+ boolean correctPin = false;
  
+ Scanner sc = new Scanner(System.in);
+ 
+ while(correctPin != true)
+ {
+  System.out.print("Please enter your four digit pin: ");
+  int input = sc.nextInt();
+  if(input == 1998)
+  {
+   correctPin = true;
+  }
+ }
 
 }
